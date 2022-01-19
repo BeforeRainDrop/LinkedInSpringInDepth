4d40782e413657a886aaaafef30f8f0c3d1906b8
BeforeRainDrop <giovanna.cimillo@gmail.com>
2022-01-19 19:23:22 +0100

    [Spring] Aspects - Before

--- src/main/java/it/giovanna/spring/config/ApplicationConfig.java
+++ src/main/java/it/giovanna/spring/config/ApplicationConfig.java
@@ -2,11 +2,13 @@ package it.giovanna.spring.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "it.giovanna.spring")
+@EnableAspectJAutoProxy
 public class ApplicationConfig {
 
 }
--- src/main/java/it/giovanna/spring/service/GreetingService.java
+++ src/main/java/it/giovanna/spring/service/GreetingService.java
@@ -3,6 +3,8 @@ package it.giovanna.spring.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import it.giovanna.spring.aspect.Loggable;
+
 @Service
 public class GreetingService {
 
@@ -13,6 +15,7 @@ public class GreetingService {
         super();
     }
 
+    @Loggable
     public String getGreeting(String name){
         return greeting + " " + name;
     }
